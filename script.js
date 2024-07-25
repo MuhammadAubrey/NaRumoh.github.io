d13be2ad83aad6d7ce2c108bb6830a6b1c66ab8c
Agung Denisa <idnmuhammadaubrey@gmail.com>
2024-07-25 19:21:06 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+function btnAbout() {
+  location.replace("aboutUs.html");
+}
+
+function btnPrice() {
+  location.replace("price.html");
+}
+
+function btnNumber() {
+  location.replace("wa.me/621281676661");
+}
